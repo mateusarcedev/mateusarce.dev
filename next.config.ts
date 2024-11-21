eb3a37c63b90708e65153a988970505e83b4902e
Mateus Arce <arcemateuss@gmail.com>
2024-11-21 00:01:55 -0400

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
+  compress: false,
   images: {
     domains: ['avatars.githubusercontent.com'],
+    unoptimized: true,
   },
 };
 
